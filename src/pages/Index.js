51eda4fd7a11c7704e8a9bfacedf59e56cb6ceb9
Nyijia Morgan <mnyijia23@gmail.com>
2023-10-05 20:51:58 -0500

    done

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,22 +1,28 @@
 import Post from '../components/Post';
 import {useLoaderData, Form} from 'react-router-dom';
 
+
 function Index (props){
 
     // fetch the loaderdata using the useLoaderData hook
-    const todos = useLoaderData()
+    const reservs = useLoaderData()
 
-    // map over the todos and create a Post component for each todo
+    // map over the todos and create a Post component for each reserv
     return <>
     <div style={{textAlign: "center"}}>
-        <h2>Create a Todo</h2>
+        <h2 className="red">Schedule Oil Change</h2>
         <Form method="post" action="/create">
-            <input type="text" name="subject" placeholder="Subject"/>
-            <input type="text" name="details" placeholder="Details"/>
-            <button>Create a new Todo</button>
+            <input type="text" name="date" placeholder="Drop-off Date"/>
+            <input type="text" name="name" placeholder="Name"/>
+            <input type="text" name="date" placeholder="Pick-up Date"/>
+            <input type="text" name="phone_number" placeholder="Phone Number"/>
+            <input type="text" name="Vin" placeholder="Vin Number"/>
+            <button>Schedule New Oil Change</button>
         </Form>
     </div>
-    {todos.map((todo) => <Post key={todo.id} post={todo}/>)}
+    <div>
+    {reservs.map((reserv) => <Post key={reserv.id} post={reserv}/>)}
+    </div>
     </>
 
 }
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -12,27 +12,48 @@ function Show(props) {
 
   return (
     <div style={div}>
-      <h1>{post.subject}</h1>
-      <h2>{post.details}</h2>
+        <h1>{post.date}</h1>
+        <h2>{post.name}</h2>
+        <h2>{post.time}</h2>
+        <h2>{post.phone_number}</h2>
+        <h2>{post.number_of_customers}</h2>
       <div style={{ textAlign: "center" }}>
-        <h2>Update Todo</h2>
+        <h2 className="red">Update Reservation</h2>
         <Form method="post" action={`/update/${post.id}`}>
           <input
             type="text"
-            name="subject"
-            placeholder="Subject"
-            defaultValue={post.subject}
+            name="date"
+            placeholder="Drop-off Date"
+            defaultValue={post.date}
           />
           <input
             type="text"
-            name="details"
-            placeholder="Details"
-            defaultValue={post.details}
+            name="name"
+            placeholder="Name"
+            defaultValue={post.name}
           />
-          <button>Update Todo</button>
+          <input
+            type="text"
+            name="date"
+            placeholder="Pick-up Date"
+            defaultValue={post.time}
+          />
+          <input
+            type="text"
+            name="phone_number"
+            placeholder="Phone Number"
+            defaultValue={post.phone_number}
+          />
+          <input
+            type="text"
+            name="vin"
+            placeholder="Vin Number"
+            defaultValue={post.number_of_customers}
+          />
+          <button>Update Oil Change</button>
         </Form>
         <Form method="post" action={`/delete/${post.id}`}>
-            <button>Delete Todo</button>
+            <button>Delete Oil Change</button>
         </Form>
       </div>
       <Link to="/">Back to Index</Link>
